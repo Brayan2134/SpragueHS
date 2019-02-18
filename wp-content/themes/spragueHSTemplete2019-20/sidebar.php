75b872aa076d5d6825753a91160493d096110c0e
Brayan Quevedo <brayanquevedo2134@gmail.com>
2019-02-18 10:11:48 -0800

    Added Sidebar with sublinks
    On this version I figured out how to make a sidebar with dropdown menus. If you click the 'Quicklinks' icon, you will see the dropdown menu in action!

--- wp-content/themes/spragueHSTemplete2019-20/sidebar.php
+++ wp-content/themes/spragueHSTemplete2019-20/sidebar.php
@@ -1,14 +1,37 @@
 <!-- Sidebar -->
 <div class="bg-light border-right" id="sidebar-wrapper">
-  <div class="sidebar-heading">Start Bootstrap</div><!--Navbar Title-->
+  <div class="sidebar-heading">Sprague High School.</div><!--Navbar Title-->
     <!--Navbar content-->
     <div class="list-group list-group-flush">
-      <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
-      <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
-      <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
-      <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
-      <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
-      <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+      <a href="index.php" class="list-group-item list-group-item-action bg-light">Home</a>
+      <a href="#" class="list-group-item list-group-item-action bg-light">Student</a>
+      <a href="#" class="list-group-item list-group-item-action bg-light">Parent</a>
+      <a href="#" class="list-group-item list-group-item-action bg-light">Information</a>
+      <a class="list-group-item list-group-item-action bg-light dropdown-btn" data-toggle="collapse" href="#collapseQuicklinks" role="button" aria-expanded="false" aria-controls="collapseQuicklinks">Quicklinks</a>
+        <!--Dropdown menu options for section 'Quicklinks'-->
+        <div class="dropdown-container collapse" id="collapseQuicklinks">
+          <a class="list-group-item list-group-item-action bg-light" href="https://sis-portal.salkeiz.k12.or.us" target="_blank">StudentVue</a>
+          <a class="list-group-item list-group-item-action bg-light" href="#">Link 2</a>
+          <a class="list-group-item list-group-item-action bg-light" href="#">Link 3</a>
+        </div>
     </div>
 </div>
 <!-- /#sidebar-wrapper -->
+
+<script>
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
+</script>
